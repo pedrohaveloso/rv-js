163db5db04107b709e316c73032b96c5c09b2025
Pedro Veloso <contatopedrohalves@gmail.com>
2024-04-07 23:55:12 -0300

    feat: data elements filter data.

--- rv/app.js
+++ rv/app.js
@@ -1,90 +1,131 @@
-import { cl } from "./utils/cl.js"
+Element.prototype.getTagOnly = function () {
+  return this.outerHTML.replace(this.innerHTML, "");
+};
 
-const dataElements = document.querySelectorAll('[\\@data]')
+/**
+ * Seleciona e armazena todos os elementos de dados.
+ *
+ * @type { NodeListOf<Element> }
+ */
+const dataElements = document.querySelectorAll("[\\@data]");
 
-function recursiveDataElement(i, dataElements) {
+dataElements.forEach((dataElement) => {
+  /**
+   * @type { string | null }
+   */
+  const elementDataAttribute = dataElement.getAttribute("@data");
 
-  const element = dataElements[index]
+  if (elementDataAttribute === null || elementDataAttribute.length === 0) {
+    console.warn(
+      "Todo elemento de dados deve ter seu atributo @data preenchido. \n @: " +
+        dataElement.getTagOnly()
+    );
 
-  const data = JSON.parse(
-    element.getAttribute('@data')
-      .replace('$', '')
-      .replace(/(\w+):/g, '"$1":')
-      .replace(/'/g, '"')
-  )
-
-  const echos = []
-
-  function recursiveElement(element, data) {
-    element.childNodes.forEach((child) => {
-      if (typeof child.tagName === 'undefined') {
-
-        return
-      }
-
-      if (child.hasAttribute('@data')) {
-
-        dataElements = [...dataElements, ...[child]]
-      }
-
-      if (child.hasAttribute('@click')) {
-
-        child.onclick = (event) => {
-          eval(child.getAttribute('@click').replaceAll('$', 'data.rv'))
-        }
-      }
-
-      if (child.tagName.toLowerCase() === 'echo') {
-
-        const attributeVar = child.attributes[0].name
-
-        const text = document.createTextNode(eval('data.' + attributeVar))
-
-        echos.push({ rv: attributeVar, fn: (value) => text.nodeValue = value })
-
-        child.after(text)
-
-        child.remove()
-      }
-
-
-      if (child.children.length >= 1) {
-
-        recursiveElement(child, data)
-      }
-    })
+    return;
   }
 
-  recursiveElement(element, data)
+  /**
+   * @type { Map<string, any> | null }
+   */
+  let elementData = null;
 
-  const dataMap = new Map(Object.entries(data))
+  try {
+    elementData = new Map(
+      Object.entries(
+        JSON.parse(
+          elementDataAttribute
+            .replace("$", "")
+            .replace(/(\w+):/g, '"$1":')
+            .replace(/'/g, '"')
+        )
+      )
+    );
+  } catch (ex) {
+    console.error(
+      "Dados do elemento inválidos. \n @: " +
+        dataElement.getTagOnly() +
+        " \n @: " +
+        elementDataAttribute
+    );
 
-  dataMap.forEach((item, key) => {
-    data[`rv${key}`] = item
+    return;
+  }
 
-    Object.defineProperty(
-      data,
-      `rv${key}`,
-      {
-        get: function () {
-          return this[key]
-        },
+  console.log(elementData);
+});
 
-        set: function (value) {
-          echos.forEach((echo) => {
-            echo.fn(value)
-          })
+// function recursiveDataElement(i, dataElements) {
 
-          this[key] = value
-        }
-      }
-    )
-  })
+//   const element = dataElements[index]
 
-  cl(data)
-  cl(echos)
-}
+//   const echos = []
 
-recursiveDataElement(0, dataElements)
+//   function recursiveElement(element, data) {
+//     element.childNodes.forEach((child) => {
+//       if (typeof child.tagName === 'undefined') {
 
-cl(dataElements)
+//         return
+//       }
+
+//       if (child.hasAttribute('@data')) {
+
+//         dataElements = [...dataElements, ...[child]]
+//       }
+
+//       if (child.hasAttribute('@click')) {
+
+//         child.onclick = (event) => {
+//           eval(child.getAttribute('@click').replaceAll('$', 'data.rv'))
+//         }
+//       }
+
+//       if (child.tagName.toLowerCase() === 'echo') {
+
+//         const attributeVar = child.attributes[0].name
+
+//         const text = document.createTextNode(eval('data.' + attributeVar))
+
+//         echos.push({ rv: attributeVar, fn: (value) => text.nodeValue = value })
+
+//         child.after(text)
+
+//         child.remove()
+//       }
+
+//       if (child.children.length >= 1) {
+
+//         recursiveElement(child, data)
+//       }
+//     })
+//   }
+
+//   recursiveElement(element, data)
+
+//   const dataMap = new Map(Object.entries(data))
+
+//   dataMap.forEach((item, key) => {
+//     data[`rv${key}`] = item
+
+//     Object.defineProperty(
+//       data,
+//       `rv${key}`,
+//       {
+//         get: function () {
+//           return this[key]
+//         },
+
+//         set: function (value) {
+//           echos.forEach((echo) => {
+//             echo.fn(value)
+//           })
+
+//           this[key] = value
+//         }
+//       }
+//     )
+//   })
+
+//   cl(data)
+//   cl(echos)
+// }
+//
